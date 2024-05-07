da22148bbc34fea51fc76e3b48ea3878b4ab3e9e
VALUE <vochuba95@gmail.com>
2024-05-07 05:45:16 -0700

    Query the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
+"""Function that queries the Reddit API and prints the top ten hot posts"""
 import requests
 import sys
 
