9c7bd76c7bca629843c744b196cc143a4a0f2163
VALUE <vochuba95@gmail.com>
2024-05-07 05:18:10 -0700

    Query the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,12 +8,6 @@ import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 10}
