04acc8e4241cc3397c0dd0c5ba28a179f3d8c064
VALUE <vochuba95@gmail.com>
2024-05-08 05:27:07 -0700

    Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fix 500 error when a GET HTTP method is requested to Apache web server.
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+
+exec {'replace':
+  provider => shell,
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
