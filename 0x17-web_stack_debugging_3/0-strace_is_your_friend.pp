2131b907b21544d8b8d019471b428519063dede9
VALUE <vochuba95@gmail.com>
2024-05-08 05:38:31 -0700

    Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,10 @@
 # Fix 500 error when a GET HTTP method is requested to Apache web server.
-
-$settings_file='/var/www/html/wp-settings.php'
-file { $settings_file:
+$fix='/var/www/html/wp-settings.php'
+file { $fix:
   ensure => file,
 }
-
 exec {'fix typo in settings config':
   path    => ['/bin/', '/usr/bin/', '/usr/sbin/'],
-  command => "sed -i s/phpp/php/g ${settings_file}",
-  require => File[$settings_file],
+  command => "sed -i s/phpp/php/g ${fix}",
+  require => File[$fix],
 }
