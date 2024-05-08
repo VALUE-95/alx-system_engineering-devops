8d19ad5185028ef075f21c0c86a23089c39f16d1
VALUE <vochuba95@gmail.com>
2024-05-08 05:19:09 -0700

    Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server.
+
 exec { 'fix_apache_error':
   command     => '/usr/sbin/service apache2 restart',
   refreshonly => true,
